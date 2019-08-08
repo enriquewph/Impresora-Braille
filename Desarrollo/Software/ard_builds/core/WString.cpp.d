c04b80dbb381e2e90a61b3a08565186f8fa3fb37
Enrique Walter Philippeaux <quique18c@gmail.com>
2019-08-08 11:51:51 -0300

    iniciado el proyecto de arduino

---
+++ Desarrollo/Software/ard_builds/core/WString.cpp.d
@@ -0,0 +1,34 @@
+c:\Users\quiqu\Documents\GitHub\Impresora-Braille\Desarrollo\Software\ard_builds\core\WString.cpp.o: \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2\cores\esp32\WString.cpp \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2\cores\esp32\WString.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/stdlib.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/machine/ieeefp.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/_ansi.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/newlib.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/sys/config.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/sys/features.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/esp32/xtensa/config/core-isa.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/sys/reent.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/_ansi.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/sys/_types.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/machine/_types.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/machine/_default_types.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/sys/lock.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/sys/cdefs.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/machine/stdlib.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/alloca.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/string.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/sys/string.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/ctype.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2\cores\esp32/pgmspace.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2\cores\esp32\stdlib_noniso.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2\cores\esp32\esp32-hal-log.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/config/sdkconfig.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/log/esp_log.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/stdint.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/sys/_intsup.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/esp32/rom/ets_sys.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/soc/soc/soc.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/esp32/esp_assert.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/assert.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/log/esp_log_internal.h
---
+++ Desarrollo/Software/ard_builds/core/libb64/cdecode.c.d
@@ -0,0 +1,7 @@
+c:\Users\quiqu\Documents\GitHub\Impresora-Braille\Desarrollo\Software\ard_builds\core\libb64\cdecode.c.o: \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2\cores\esp32\libb64\cdecode.c \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2\cores\esp32\libb64\cdecode.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/stdint.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/machine/_default_types.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/sys/features.h \
+ C:\Users\quiqu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.2/tools/sdk/include/newlib/sys/_intsup.h
