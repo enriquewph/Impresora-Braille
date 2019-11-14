f200db519479dafd67b7c11d48e6dd4a40658460
Enrique Walter Philippeaux <quique18c@gmail.com>
2019-11-14 10:50:09 -0300

    update2

--- ard_builds/core/hooks.c.d
+++
@@ -1,2 +0,0 @@
-c:\Users\quiqu\Documents\GitHub\Impresora-Braille\ard_builds\core\hooks.c.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\hooks.c
--- ard_builds/core/new.cpp.d
+++
@@ -1,2 +0,0 @@
-c:\Users\quiqu\Documents\GitHub\Impresora-Braille\ard_builds\core\new.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\new.cpp
